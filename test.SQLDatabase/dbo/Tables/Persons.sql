dec6f34331c9cc67be3fb85ab576258b4804eaf1
Siyang Yao <ysy68251435@gmail.com>
2025-03-25 10:52:38 -0700

    I'm committing all my changes in dxt.

---
+++ test.SQLDatabase/dbo/Tables/Persons.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[Persons] (
+    [ID]        INT           NOT NULL,
+    [LastName]  VARCHAR (255) NOT NULL,
+    [FirstName] VARCHAR (255) NULL,
+    [Age]       INT           NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+
